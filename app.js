1f08b1f3d0bb0c03ddf439f6a1532d45ce9f36d4
maratanka <anna.markowicz5@gmail.com>
2017-07-21 11:38:21 +0000

    Destroy route

--- app.js
+++ app.js
@@ -93,6 +93,20 @@ app.put("/blogs/:id", function(req, res){
    });
 });
 
+// DELETE ROUTE
+
+app.delete("/blogs/:id", function(req, res){
+    //destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs")
+        } else {
+            res.redirect("/blogs")
+        }
+    });
+    //redirect somewhere
+});
+
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Server is running!"); 
 });
