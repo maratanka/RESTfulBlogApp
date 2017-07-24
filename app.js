3537243dff1e81b2e0981b6f6a364715a7585d59
maratanka <anna.markowicz5@gmail.com>
2017-07-24 09:12:57 +0000

    Final changes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var bodyParser = require("body-parser"),
     methodOverride = require("method-override"),
+    expressSanitizer = require("express-sanitizer"),
     mongoose = require("mongoose"),
     express = require("express"),
     app = express();
@@ -9,6 +10,7 @@ mongoose.connect("mongodb://localhost/restful_blog_app");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
 app.use(methodOverride("_method"));
 
 // MONGOOSE/MODEL CONFIG
@@ -46,6 +48,10 @@ app.get("/blogs/new", function(req, res){
 
 app.post("/blogs", function(req, res){
    //create blog
+   console.log(req.body);
+   req.body.blog.body = req.sanitize(req.body.blog.body);
+   console.log("===============");
+   console.log(req.body);
    Blog.create(req.body.blog, function(err, newBlog){
        if(err){
            res.render("new");
@@ -84,6 +90,7 @@ app.get("/blogs/:id/edit", function(req, res) {
 // UPDATE ROUTE
 
 app.put("/blogs/:id", function(req, res){
+   req.body.blog.body = req.sanitize(req.body.blog.body);
    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
       if(err){
           res.redirect("/blogs");
